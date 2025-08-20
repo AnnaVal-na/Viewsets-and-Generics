997afb7a0bcaa528e995cda91f9eb0fb15651861
Your Name <you@example.com>
2025-08-20 19:48:02 +0300

    Viewsets_and_Generics_3

--- courses/views.py
+++ courses/views.py
@@ -1,21 +1,8 @@
-# Создаем ViewSet для курсов
-from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Course
-from .serializers import CourseSerializer
-# Создаем Generic-классы для уроков
 from rest_framework import generics
 from .models import Lesson
 from .serializers import LessonSerializer
 
 
-# Создаем ViewSet для курсов
-class CourseViewSet(viewsets.ModelViewSet):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
-# Создаем Generic-классы для уроков
 class LessonListAPIView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
@@ -27,7 +14,6 @@ class LessonRetrieveAPIView(generics.RetrieveAPIView):
 
 
 class LessonCreateAPIView(generics.CreateAPIView):
-    queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
 
@@ -38,4 +24,3 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
