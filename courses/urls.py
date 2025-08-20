b31d03e8ebe5052d7ed7ccc7af252e4fb542b59f
Your Name <you@example.com>
2025-08-20 21:11:38 +0300

    Viewsets_and_Generics_4.1

--- courses/urls.py
+++ courses/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import CourseViewSet, LessonListAPIView, LessonRetrieveAPIView, LessonCreateAPIView, LessonUpdateAPIView, LessonDestroyAPIView
 
@@ -6,10 +6,11 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('lessons/', LessonListAPIView.as_view(), name='lesson-list'),
     path('lessons/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
     path('lessons/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-detail'),
     path('lessons/<int:pk>/update/', LessonUpdateAPIView.as_view(), name='lesson-update'),
     path('lessons/<int:pk>/delete/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ]
+
+urlpatterns += router.urls
