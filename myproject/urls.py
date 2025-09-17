01b2d4afcbe3b84d3c6270dca09c4bf906b73316
Your Name <you@example.com>
2025-09-17 13:36:25 +0300

    CI_CD_GitHub_Actions

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,6 +18,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('courses.urls')),
