ce5508fe579b171dcec3750aba3a04530bde7020
Your Name <you@example.com>
2025-10-01 12:12:46 +0300

    fix: remove obsolete version and sync DB password

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from .views import RootView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -20,6 +21,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', RootView.as_view(), name='root'),
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("courses.urls")),
